24b8622c2ef02bc64a7eedaaacdb29a47fa4071c
tomaszociepa <tom.ociepa@gmail.com>
2019-05-24 09:16:48 +0200

    exercise 7 - create random table

---
+++ src/Exercise7.java
@@ -0,0 +1,73 @@
+
+// 7. Dana jest wejściowa tablica integerów. Należy znaleźć czy istnieją pary liczb sumujące się do podanej sumy (t). Jeśli tak, należy
+// usunąć drugą liczbę z pary z tablicy i zwrócić tablicę wynikową. Przykład:
+//
+// x = [1, 2, 3, 4, 5] t = 3
+//
+// 1+2 = t, więc wyrzucamy 2. Żadna inna para nie sumuje się do t, więc wynikiem jest: [1, 3, 4, 5]
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Exercise7 {
+
+    public static void main(String[] args) {
+
+        int[] randomTable = new int[5];
+        Random random = new Random();
+
+        for (int i = 0; i < randomTable.length; i++) {
+            randomTable[i] = random.nextInt(10);
+        }
+
+        System.out.println("Dana jest tablica: ");
+
+        for (int i = 0; i < randomTable.length; i++) {
+
+            if (i == 0) {
+                System.out.print("[" + randomTable[i] + ", ");
+            } else if (i == 4) {
+                System.out.println(randomTable[i] + "]");
+            } else {
+                System.out.print(randomTable[i] + ", ");
+            }
+        }
+
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("Podaj sumę (t): ");
+        int t = in.nextInt();
+        int suma = 0;
+        int index1 = -1;
+        int index2 = -1;
+
+        for (int i = 0; i < 5; i++) {
+            if(i != 4){
+
+                suma = randomTable[i] + randomTable[i + 1];
+
+                if (suma == t){
+                    index1 = i;
+                    index2 = i + 1;
+                }
+
+            } else if (i == 4){
+                suma = randomTable[i] + randomTable[i - 1];
+
+                if (suma == t){
+                    index1 = i - 1;
+                    index2 = i;
+                }
+            }
+        }
+
+        System.out.println("index to: " + index1 + " oraz "+ index2);
+        
+
+        System.out.println("Nowa tablica to: ");
+
+
+
+
+    }
+}
